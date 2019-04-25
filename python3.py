fc72608690adfffab1e176ca33072bc7c0a635b9
AmandaSacchi <amandasacchi@hotmail.com>
2019-04-25 09:08:13 -0300

    Aula 03

---
+++ python3.py
@@ -0,0 +1,23 @@
+class Veiculo (cor,rodas,motor,assento,porta):
+    self.cor = cor
+    self.rodas = rodas
+    self.motor = motor
+    self.assento = assento
+    self.porta = porta
+
+caminhao 
+caminhao.cor = "bege"
+caminhao.rodas = 4
+caminhao.porta = "manual"
+
+ferrari
+ferrari.cor = "vermelho"
+ferrari.rodas = 4
+ferrari.porta = "eletrico"
+
+fusca
+fusca.cor = "preto"
+fusca.rodas = 4
+fusca.porta = 
+
+
