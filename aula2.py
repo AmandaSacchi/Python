aa832c297186b0a8d7fe8ae56def978a023ede83
AmandaSacchi <amandasacchi@hotmail.com>
2019-04-25 09:11:44 -0300

    Aula 02

---
+++ aula2.py
@@ -0,0 +1,6 @@
+def soma(valor1, valor2):
+    return valor1 + valor2
+
+valor1 = int(input("Qual a sua idade?"))
+valor2 = int(input("Qual a sua idade?"))
+print (f"A soma eh {soma(valor1, valor2)}")  
